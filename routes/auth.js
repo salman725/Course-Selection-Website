7958c470cb31b56b58953c98f0aae1e0ad2c9869
salman725 <60856525+salman725@users.noreply.github.com>
2020-12-02 23:37:21 -0500

    hashed passwords, added error messages, other login backend functionalities

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const auth = require('express').Router();
 const User = require('../models/User');
-const { registerValidation }= require('../validation')
+const bcrypt = require('bcryptjs');
+const { registerValidation, loginValidation}= require('../validation')
 
+// Register
 auth.post('/register', async (req,res) => {
 
     //Let's validate the data before we make a user
@@ -12,18 +14,41 @@ auth.post('/register', async (req,res) => {
     const emailExist = await User.findOne({email: req.body.email});
     if (emailExist) return res.status(400).send('Email already exists');
 
+    //HASH passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //Create new user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
-        res.json(savedUser);
+        res.json({user: user._id});
     }catch(err){
         res.status(400).json(err);
     }
+});
+
+//Login
+auth.post('/login', async (req,res) => {
+
+    //Let's validate the data before we make a user
+    const {error} = loginValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the email exists
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Email is not found');
+
+    //Password is correct or not
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if(!validPass) return res.status(400).send('Invalid password');
+
+    res.send('Logged in!');
+
 });
 
 module.exports = auth;
